c2313f8afc509e7a289395758214886d1e2416ed
Shreya1720 <36411675+Shreya1720@users.noreply.github.com>
2021-08-24 13:56:16 +0530

    Update STRING_PRACTICE.sql

--- SQL/Shreya/DAY-3/STRING_PRACTICE.sql
+++ SQL/Shreya/DAY-3/STRING_PRACTICE.sql
@@ -5,7 +5,7 @@ SELECT ASCII('a') AS 'a' , ASCII('A') AS 'A'
 SELECT CHAR(97) 
 
 --UNICODE ITS RETURN THE ASCII VALUE (ASCII FUNCTION IS USED FOR ONLY 7-BIT STREAM)
-SELECT UNICODE('æ')
+SELECT UNICODE('Ã¦')
 
 --return the index number of character which we want to find 
 SELECT CHARINDEX('a', 'Shreya Thakkar')
@@ -70,4 +70,35 @@ SELECT SUBSTRING('SHREYA',2,5)
 
 --SELECT STRING_SPLIT('SHREYA THAKKAR','')
 SELECT FORMAT (123456678,'##-###-####')
+
+
+
+
+select 'a'+char(13)+'b'
+
+
+SELECT CAST(0x81F4 AS CHAR(2)) 
+
+SELECT NCHAR(127925)
+
+select 'a'+char(10)+'b'
+
+SELECT CHARINDEX('S','MICROSOFT SERVER',7)
+
+SELECT CHARINDEX('S','MICROSOFT SERVER',CHARINDEX('S','MICROSOFT SERVER')+1)
+
+SELECT FORMAT(SYSDATETIME(), 'hh:mm tt'); -- returns 03:46 PM
+SELECT FORMAT(SYSDATETIME(), 'hh:mm t'); -- returns 03:46 P
+
+SELECT value FROM STRING_SPLIT('Lorem ipsum dolor sit amet.', ' ');
+
+DECLARE @tags NVARCHAR(400) = 'clothing,road,,touring,bike'  
+  
+SELECT value  
+FROM STRING_SPLIT(@tags, ',')  
+WHERE RTRIM(value) <> '';
+
+
+DECLARE @t time = GETDATE();   
+SELECT DATENAME(Y, @t);
 
